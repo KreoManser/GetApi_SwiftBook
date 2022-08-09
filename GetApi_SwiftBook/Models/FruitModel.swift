71b03c7a01eb6722f81cc567d6b2462493d72522
KreoManser <sherba.ru@list.ru>
2022-08-09 14:31:21 +0700

    setup and complete task

---
+++ GetApi_SwiftBook/Models/FruitModel.swift
@@ -0,0 +1,25 @@
+//
+//  FruitModel.swift
+//  GetApi_SwiftBook
+//
+//  Created by Сергей Бабич on 09.08.2022.
+//
+
+import Foundation
+
+struct Fruit: Decodable {
+    let genus: String?
+    let name: String?
+    let id: Int?
+    let family: String?
+    let order: String?
+    let nutritions: Nutritions?
+}
+
+struct Nutritions: Decodable {
+    let carbohydrates: Double?
+    let protein: Double?
+    let fat: Double?
+    let calories: Double?
+    let sugar: Double?
+}
